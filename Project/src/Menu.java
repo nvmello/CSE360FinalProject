6a099891fc608be03d63cbbab1ad8364416b5e0d
ppwee <lamanhlengo@gmail.com>
2020-11-25 13:28:22 -0700

    added

--- Project/src/Menu.java
+++ Project/src/Menu.java
@@ -1,15 +1,16 @@
 import javax.swing.*;
-import javax.swing.filechooser.FileNameExtensionFilter;
-
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.io.File;
 import java.util.Scanner;  // Import the Scanner class
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
 public class Menu extends JFrame implements ActionListener {
+    JFrame jf;
+    JFileChooser fc;
 
-	 JFrame jf;
-	 
     public Menu() {
         jf = new JFrame();
         jf.setTitle("CSE360 Final Project");
@@ -17,26 +18,44 @@ public class Menu extends JFrame implements ActionListener {
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jf.setVisible(true);
 
+        fc = new JFileChooser();
+
         JMenuBar jmb = new JMenuBar();
         jf.setJMenuBar(jmb);
 
         JMenu file = new JMenu("File");
         jmb.add(file);
-        JMenuItem load = new JMenuItem("Load Roster");
-        file.add(load);
-        file.addSeparator();
-        JMenuItem addAtt = new JMenuItem("Add Attendance");
-        file.add(addAtt);
-        load.addActionListener(this);
-        file.addSeparator();
-        JMenuItem save = new JMenuItem("Save");
-        file.add(save);
-        file.addSeparator();
-        JMenuItem plot = new JMenuItem("Plot Data");
-        file.add(plot);
 
         JMenu about = new JMenu("About");
         jmb.add(about);
+
+        JMenuItem load = new JMenuItem("Load Roster", KeyEvent.VK_O);
+        file.add(load);
+        load.setActionCommand("Load Roster");
+        load.addActionListener(this);
+        file.addSeparator();
+
+        JMenuItem addAtt = new JMenuItem("Add Attendance");
+        file.add(addAtt);
+        addAtt.setActionCommand("Add Attendance");
+        addAtt.addActionListener(this);
+        file.addSeparator();
+
+
+        JMenuItem save = new JMenuItem("Save");
+        file.add(save);
+        save.setActionCommand("Save");
+        save.addActionListener(this);
+        file.addSeparator();
+
+
+        JMenuItem plot = new JMenuItem("Plot Data");
+        file.add(plot);
+        plot.setActionCommand("Plot Data");
+        plot.addActionListener(this);
+
+
+
     }
 
 
@@ -49,26 +68,39 @@ public class Menu extends JFrame implements ActionListener {
      * File path on Nicks PC for testing: " C:\Users\nvmor\Desktop\input1.csv "
      */
     public void actionPerformed(ActionEvent e) {
-        Scanner input = new Scanner(System.in);  // Create a Scanner object
-        LoadRoster newRost = new LoadRoster();  //Creates a new load roster obj
-        
-        
-        JFileChooser fileChooser = new JFileChooser(); //JFileChooser points to user's default directory
-        FileNameExtensionFilter filter = new FileNameExtensionFilter("Only .csv files","csv"); 
-        fileChooser.setFileFilter(filter);	//only allowing csv file extensions
-        
-        int result = fileChooser.showOpenDialog(null);    
-        if(result == JFileChooser.APPROVE_OPTION) {
-        
-          	newRost.readCSV(fileChooser.getSelectedFile());	//passing in the selected file to be read
+        if("Load Roster".equals(e.getActionCommand())){
+            Scanner input = new Scanner(System.in);  // Create a Scanner object
+            LoadRoster newRost = new LoadRoster();  //Creates a new load roster obj
+
+
+            JFileChooser fileChooser = new JFileChooser(); //JFileChooser points to user's default directory
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("Only .csv files","csv");
+            fileChooser.setFileFilter(filter);	//only allowing csv file extensions
+
+            int result = fileChooser.showOpenDialog(null);
+            if(result == JFileChooser.APPROVE_OPTION) {
+
+                newRost.readCSV(fileChooser.getSelectedFile());	//passing in the selected file to be read
+            }
+
+            String[] headers = {"ID","First Name","Last Name","Program and Plan","Academic Level","ASURITE"};
+
+            JTable t = new JTable(newRost.getData(),headers); //filling in the JTable with file info
+
+            JScrollPane js = new JScrollPane(t);
+            jf.add(js);
         }
-        
-        String[] headers = {"ID","First Name","Last Name","Program and Plan","Academic Level","ASURITE"}; 
-        
-        JTable t = new JTable(newRost.getData(),headers); //filling in the JTable with file info 
-        
-        JScrollPane js = new JScrollPane(t);
-        jf.add(js);
-        
+
+        if("Add Attendance".equals(e.getActionCommand())){
+            System.out.println("It works2!");
+        }
+
+        if("Save".equals(e.getActionCommand())){
+            System.out.println("It works!3");
+        }
+        if("Plot Data".equals(e.getActionCommand())){
+            System.out.println("It works!4");
+        }
+
     }
 }
